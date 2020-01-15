16710deba240a0c3e7be30978434ac4d87ea163d
randallkk <randallkk@sookmyung.ac.kr>
2020-01-15 21:08:42 +0900

    완료

--- sentiment_analysis/p_analysis.py
+++ sentiment_analysis/p_analysis.py
@@ -8,14 +8,14 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
-def print_result(annotations):
+'''def print_result(annotations):
     score = annotations.document_sentiment.score
     magnitude = annotations.document_sentiment.magnitude
 
     for index, sentence in enumerate(annotations.sentences):
         sentence_sentiment = sentence.sentiment.score
         if(sentence_sentiment > 0):
-            print(sentence_sentiment)
+            print(index)
         #print('Sentence {} has a sentiment score of {}'.format(
         #    index, sentence_sentiment))
 
@@ -37,7 +37,7 @@ def analyze(movie_review_filename):
     annotations = client.analyze_sentiment(document=document)
 
     # Print the results
-    print_result(annotations)
+    print_result(annotations)'''
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -48,4 +48,29 @@ if __name__ == '__main__':
         help='The filename of the movie review you\'d like to analyze.')
     args = parser.parse_args()
 
-    analyze(args.movie_review_filename)
+    """Run a sentiment analysis request on text within a passed filename."""
+    client = language.LanguageServiceClient()
+
+    with open(args.movie_review_filename, 'r', encoding='UTF-8') as review_file:
+        # Instantiates a plain text document.
+        content = review_file.read()
+        want = review_file.readlines()
+
+    document = types.Document(
+        content=content,
+        type=enums.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(document=document)
+
+    # Print the results
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        if(sentence_sentiment > 0):
+            print(index)
+        #print('Sentence {} has a sentiment score of {}'.format(
+        #    index, sentence_sentiment))
+
+    print('Overall Sentiment: score of {} with magnitude of {}'.format(
+        score, magnitude))
